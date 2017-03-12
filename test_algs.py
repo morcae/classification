70eace4876da4ba554118c8e3c23e11282bea667
Elena Tsykunova <morcae@users.noreply.github.com>
2017-03-12 20:14:04 +0300

    Update test_algs.py

--- test_algs.py
+++ test_algs.py
@@ -1,5 +1,4 @@
 from python_speech_features import mfcc
-from python_speech_features import logfbank
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
@@ -10,32 +9,29 @@ from sklearn.linear_model import RidgeClassifier
 import scipy.io.wavfile as wav
 import os
 import numpy as np
-
+import time
 
 
 def getXYtrainset():
-    ''' getting features from train set'''
     path = "C:/Users/Lena/PycharmProjects/nir/train/"
     X = []
-    Y = ['b']*90 + ['c']*90 + ['p']*90 + ['r']*90   # 4 classes: blues, classical, pop, rock
+    Y = ['b']*90 + ['c']*90 + ['p']*90 + ['r']*90
 
     for filename in os.listdir(path):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate,sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X.append(fbank_feat[1:3,:])
-
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X.append(mfcc_feat[:])
     X = np.array(X)
-    dataset_size = len(X)
-    newX = X.reshape(dataset_size, -1)
-    return newX, Y
-
+    Y = np.array(Y)
+    return X, Y
 
 def predict_naiveb(x,y):
-    ''' naive bayesian classifier'''
     model = GaussianNB()
-    m = model.fit(x, y)
+    model.fit(x, y)
     i = 0
     path = "C:/Users/Lena/PycharmProjects/nir/test/"
 
@@ -43,19 +39,17 @@ def predict_naiveb(x,y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        mfcc_feat = mfcc(sig, rate)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = model.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = model.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
     print "total predicted correct: ", i
 
 def predict_SVM(x,y):
-    ''' Support Vector Machine classifier'''
     clf = SVC()
     clf.fit(x, y)
     i = 0
@@ -65,11 +59,11 @@ def predict_SVM(x,y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = clf.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
@@ -85,11 +79,11 @@ def predict_NU_SVM(x, y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = clf.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
@@ -105,18 +99,17 @@ def predict_linear_SVM(x, y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = lin_clf.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = lin_clf.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
     print "total predicted correct: ", i
 
 def predict_neighbours(x, y):
-    '''k nearest neighbors classifier'''
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(x, y)
     i = 0
@@ -126,18 +119,17 @@ def predict_neighbours(x, y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = neigh.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = neigh.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
     print "total predicted correct: ", i
 
 def predict_RF(x, y):
-    '''Random Forest'''
     clf = RandomForestClassifier(n_estimators=10)
     clf.fit(x, y)
     i = 0
@@ -147,18 +139,17 @@ def predict_RF(x, y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = clf.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
     print "total predicted correct: ", i
 
 def predict_RC(x, y):
-    '''ridge classifier'''
     clf = RidgeClassifier()
     clf.fit(x, y)
     i = 0
@@ -168,16 +159,15 @@ def predict_RC(x, y):
         current_file = path + "%s" % filename
         print "working with " + current_file
         (rate, sig) = wav.read(current_file)
-        fbank_feat = logfbank(sig, rate)
-        X = np.array(fbank_feat[1:3,:])
-        newX = np.reshape(X, len(X[0])+len(X[1]))
-
-        predicted = clf.predict(newX)
+        mfcc_feat = mfcc(sig,rate)
+        mfcc_feat = np.reshape(mfcc_feat, -1)
+        mfcc_feat = mfcc_feat[:77974]
+        X = np.array(mfcc_feat)
+        predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
         if predicted[0] == filename[0]:
             i += 1
     print "total predicted correct: ", i
-
 
 
 
