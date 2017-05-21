cc39ac6c93ae1865926f53e9775a7a8e02417934
Elena Tsykunova <morcae@users.noreply.github.com>
2017-05-21 18:33:03 +0300

    Update test_algs.py

--- test_algs.py
+++ test_algs.py
@@ -1,22 +1,24 @@
 from python_speech_features import mfcc
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
-from sklearn.svm import NuSVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import RidgeClassifier
 import scipy.io.wavfile as wav
 import os
 import numpy as np
-import time
 
 
-def getXYtrainset():
-    path = "C:/Users/Lena/PycharmProjects/nir/train/"
+
+
+def getXYtrainset(path):
     X = []
-    Y = ['b']*90 + ['c']*90 + ['p']*90 + ['r']*90
-
+    Y = ['b']*80 + ['c']*80 + ['p']*80 + ['r']*80
     for filename in os.listdir(path):
         current_file = path + "%s" % filename
         print "working with " + current_file
@@ -29,15 +31,12 @@ def getXYtrainset():
     Y = np.array(Y)
     return X, Y
 
-def predict_naiveb(x,y):
+
+def predict_naiveb(x, y, path, folder=False):
     model = GaussianNB()
     model.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -45,19 +44,22 @@ def predict_naiveb(x,y):
         X = np.array(mfcc_feat)
         predicted = model.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = model.predict(X)
+            print current_file, " predicted to be ", predicted
 
-def predict_SVM(x,y):
+def predict_SVM(x, y, path, folder=False):
     clf = SVC()
     clf.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -65,39 +67,22 @@ def predict_SVM(x,y):
         X = np.array(mfcc_feat)
         predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = clf.predict(X)
+            print current_file, " predicted to be ", predicted
 
-def predict_NU_SVM(x, y):
-    clf = NuSVC()
-    clf.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
-        (rate, sig) = wav.read(current_file)
-        mfcc_feat = mfcc(sig,rate)
-        mfcc_feat = np.reshape(mfcc_feat, -1)
-        mfcc_feat = mfcc_feat[:77974]
-        X = np.array(mfcc_feat)
-        predicted = clf.predict(X)
-        print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
-
-def predict_linear_SVM(x, y):
+def predict_linear_SVM(x, y, path, folder=False):
     lin_clf = LinearSVC()
     lin_clf.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -105,19 +90,22 @@ def predict_linear_SVM(x, y):
         X = np.array(mfcc_feat)
         predicted = lin_clf.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = lin_clf.predict(X)
+            print current_file, " predicted to be ", predicted
 
-def predict_neighbours(x, y):
+def predict_neighbours(x, y, path, folder=False):
     neigh = KNeighborsClassifier(n_neighbors=3)
     neigh.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -125,19 +113,22 @@ def predict_neighbours(x, y):
         X = np.array(mfcc_feat)
         predicted = neigh.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = neigh.predict(X)
+            print current_file, " predicted to be ", predicted
 
-def predict_RF(x, y):
+def predict_RF(x, y, path, folder=False):
     clf = RandomForestClassifier(n_estimators=10)
     clf.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -145,19 +136,22 @@ def predict_RF(x, y):
         X = np.array(mfcc_feat)
         predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = clf.predict(X)
+            print current_file, " predicted to be ", predicted
 
-def predict_RC(x, y):
+def predict_RC(x, y, path, folder=False):
     clf = RidgeClassifier()
     clf.fit(x, y)
-    i = 0
-    path = "C:/Users/Lena/PycharmProjects/nir/test/"
-
-    for filename in os.listdir(path):
-        current_file = path + "%s" % filename
-        print "working with " + current_file
+    if folder == False:
+        current_file = path
         (rate, sig) = wav.read(current_file)
         mfcc_feat = mfcc(sig,rate)
         mfcc_feat = np.reshape(mfcc_feat, -1)
@@ -165,18 +159,132 @@ def predict_RC(x, y):
         X = np.array(mfcc_feat)
         predicted = clf.predict(X)
         print current_file, " predicted to be ", predicted
-        if predicted[0] == filename[0]:
-            i += 1
-    print "total predicted correct: ", i
+    else:
+        for filename in os.listdir(path):
+            current_file = path + "%s" % filename
+            (rate, sig) = wav.read(current_file)
+            mfcc_feat = mfcc(sig,rate)
+            mfcc_feat = np.reshape(mfcc_feat, -1)
+            mfcc_feat = mfcc_feat[:77974]
+            X = np.array(mfcc_feat)
+            predicted = clf.predict(X)
+            print current_file, " predicted to be ", predicted
 
 
 
-x, y = getXYtrainset()
 
-predict_naiveb(x, y)      #total predicted correct:  19
-predict_SVM(x, y)         #total predicted correct:  23
-predict_NU_SVM(x, y)      #total predicted correct:  24
-predict_linear_SVM(x, y)  #total predicted correct:  19
-predict_neighbours(x, y)  #total predicted correct:  23(1), 23(2), 26 (3), 24(4)
-predict_RF(x, y)          #total predicted correct:  21
-predict_RC(x, y)          #total predicted correct:  27
+def main():
+    train = str(raw_input("Welcome to music genre classification. If you want to change training set type 'yes' (or type 'no' if you don't): "))
+    if train == 'no':
+        path_train = "C:/Users/Lena/PycharmProjects/nir/train/"
+        answer = raw_input("Type 'file' or 'folder' if you want to classify one or multiple files. ")
+        if answer == 'file':
+            path = raw_input("Type path to the file: ")
+            print "Supported ml algorithms: naive bayesian, SVM, linear SVM, neighbours, random forest and ridge classifier."
+            alg = raw_input("Choose algorithm for your classification (nb, svm, lsvm, knn, rf, rc or all): ")
+            if alg == 'nb':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with naive bayesian."
+                predict_naiveb(x, y, path)
+            elif alg == 'svm':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with SVM."
+                predict_SVM(x, y, path)
+            elif alg == 'lsvm':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with linear SVM."
+                predict_linear_SVM(x, y, path)
+            elif alg == 'knn':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with neighbours."
+                predict_neighbours(x, y, path)
+            elif alg == 'rf':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with random forest."
+                predict_RF(x, y, path)
+            elif alg == 'rc':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with ridge classifier."
+                predict_RC(x, y, path)
+            elif alg == 'all':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with naive bayesian."
+                predict_naiveb(x, y, path)
+                print "Predicting with SVM."
+                predict_SVM(x, y, path)
+                print "Predicting with linear SVM."
+                predict_linear_SVM(x, y, path)
+                print "Predicting with neighbours."
+                predict_neighbours(x, y, path)
+                print "Predicting with random forest."
+                predict_RF(x, y, path)
+                print "Predicting with ridge classifier."
+                predict_RC(x, y, path)
+
+        elif answer == 'folder':
+            path = raw_input("Type path to the folder: ")
+            print "Supported ml algorithms: naive bayesian, SVM, linear SVM, neighbours, random forest and ridge classifier."
+            alg = raw_input("Choose algorithm for your classification (nb, svm, lsvm, knn, rf, rc or all): ")
+            if alg == 'nb':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with naive bayesian."
+                predict_naiveb(x, y, path, folder=True)
+            elif alg == 'svm':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with SVM."
+                predict_SVM(x, y, path, folder=True)
+            elif alg == 'lsvm':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with linear SVM."
+                predict_linear_SVM(x, y, path, folder=True)
+            elif alg == 'knn':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with neighbours."
+                predict_neighbours(x, y, path, folder=True)
+            elif alg == 'rf':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with random forest."
+                predict_RF(x, y, path, folder=True)
+            elif alg == 'rc':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with ridge classifier."
+                predict_RC(x, y, path, folder=True)
+            elif alg == 'all':
+                print "Training."
+                x, y = getXYtrainset(path_train)
+                print "Predicting with naive bayesian."
+                predict_naiveb(x, y, path, folder=True)
+                print "Predicting with SVM."
+                predict_SVM(x, y, path, folder=True)
+                print "Predicting with linear SVM."
+                predict_linear_SVM(x, y, path, folder=True)
+                print "Predicting with neighbours."
+                predict_neighbours(x, y, path, folder=True)
+                print "Predicting with random forest."
+                predict_RF(x, y, path, folder=True)
+                print "Predicting with ridge classifier."
+                predict_RC(x, y, path, folder=True)
+        else:
+            print "Wrong input."
+    else:
+        continue
+        '''rain_path = raw_input("Plese type the path to the train set: ")
+        getXYtrainset_new(train_path)
+        #ask for classes
+        #return song-class info
+        '''
+
+main()
