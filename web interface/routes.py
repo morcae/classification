07f245bac592376a0442efcbcef40e8b46c8fe60
Elena Tsykunova <morcae@users.noreply.github.com>
2017-06-07 19:41:48 +0300

    Create routes.py

---
+++ web interface/routes.py
@@ -0,0 +1,109 @@
+from bottle import route, view, Bottle, request
+from datetime import datetime
+from bottle.ext.sqlite import Plugin
+import sqlite3 as sql
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mpld3
+import plotly.plotly as py
+from plotly.graph_objs import *
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import os
+import random
+import pandas as pd
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
+@route('/contact')
+@view('contact')
+def contact():
+    """Renders the contact page."""
+    return dict(
+        title='Contact',
+        message='Your contact page.',
+        year=datetime.now().year
+    )
+
+@route('/about')
+@view('about')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='About',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
+
+@route('/second',method=['GET', 'POST'],)
+@view('second')
+def second():
+
+    # write expert's chouce to DB
+    opt = request.forms.get('genre')
+    con = sql.connect('classification.db')
+    cur = con.cursor()
+    
+    filename  = audio
+    n = cur.execute('''SELECT experts.%s from experts where id = "%s"''' %(opt, filename) )
+    n =  n.fetchone()[0] + 1
+    cur.execute('''UPDATE experts SET %s = %s WHERE id  = "%s"''' %(opt, n, filename) )
+    con.commit()
+
+    # get all scores for filename
+    scores = cur.execute('''SELECT * from experts where id = "%s"''' %filename ).fetchall()
+    y = scores[0][2:]
+    
+    # make plot with scores
+    py.sign_in("morcae", "mATYBYmeS9FS6jRNXyrz")
+    #N = len(y)
+    x = [1, 2, 3, 4]
+    #width = 0.35    data = 
+    mpl_fig = plt.figure()
+    fig, ax = plt.subplots()
+    fig.canvas.draw()
+    ax.set_ylabel('Scores')
+    ax.set_xlabel('Genres')
+    ax.set_title('Scores by experts and genres')
+    labels = ['blues', 'classical', 'pop', 'rock']
+    plt.bar(x, y, color="blue", align='center')
+    #plt.xticks(x, data)
+    fig = plt.gcf() 
+    plt.xticks(x, labels)
+    plot_url = py.plot_mpl(fig, filename='mpl-basic-bar', auto_open=False)
+    
+    con.close()
+
+    return dict(
+        title='Second',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
+
+@route('/player', method='POST')
+@view('player')
+def player():
+    # get filename with low scores from db for streaming
+    path = "/static/experts/"
+    con = sql.connect('classification.db')
+    cur = con.cursor()
+    id = cur.execute(''' SELECT id, experts.blues+experts.classical+experts.pop+experts.rock as total  FROM experts GROUP BY id ORDER BY total ASC LIMIT 1 '''  )
+    id = id.fetchone()[0]
+    con.close()
+    
+    # to use inside "second"
+    global audio
+    audio = id
+    
+    path_audio = path + audio
+
+    return dict(
+        title='Start',
+        message='Your application description page.',
+        year=datetime.now().year, path_audio = path_audio
+    )
